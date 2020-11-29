656da87e11bdc3fe0e240baf8c6b4d40a130d89e
miatran1109 <miatran1109@gmail.com>
2020-11-29 18:36:20 +0700

    crawl on usgs

---
+++ redriver/README.md
@@ -0,0 +1,17 @@
+Topic 3: Develop a website to monitor numerical data of water surface quality of the
+Red River <br>
+<ul>
+<li> Develop a crawler using Python to crawl data of water surface quality of the Red
+River from multiple data sources on the Web
+<li> The crawled data are stored in a relational database
+<li> The stored data are visualized in a web interface
+<li> Write a tutorial report to describe your crawler, your dataset and your web
+interface
+</ul>
+
+<br>Install: <br>
+`pip install beautifulsoup4` <br>
+`pip install lxml` <br>
+`pip install html5lib` <br>
+`pip install request` <br>
+`pip install mysql-connector-python` <br>
---
+++ redriver/usgs.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+import requests
+import mysql.connector
+
+source = requests.get('https://waterdata.usgs.gov/nwis/uv?cb_00010=on&cb_00060=on&cb_00065=on&cb_00095=on&cb_00300=on&cb_00400=on&cb_63680=on&format=html&site_no=05054000&period=&begin_date=2020-11-21&end_date=2020-11-28').text
+soup = BeautifulSoup(source, 'lxml')
+
+# connect to mySQL db
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="", # enter your password
+  # create database on mysql and connect  
+  database = "redriver" 
+)
+
+mycursor = mydb.cursor()
+
+# or create database
+# mycursor.execute("CREATE DATABASE redriver")
+
+# create table
+mycursor.execute("DROP TABLE IF EXISTS usgs")
+mycursor.execute("CREATE TABLE usgs(id INT PRIMARY KEY AUTO_INCREMENT ,date_time VARCHAR(100), discharge_ft3_per_s VARCHAR(100), disolved_oxygen_mg_per_L VARCHAR(100),  gege_height_feet VARCHAR(100), temperature_water_degC VARCHAR(100), specific_conductance_unfus_per_cm_at_25degC VARCHAR(100), pH_water_unfltr_field_std_units VARCHAR(100), turbidity_ir_led_light_det_ang_90_deg VARCHAR(100))")
+# just to check 
+# mycursor.execute("DESCRIBE usgs")
+# for x in mycursor:
+#     print(x)
+
+# find table stored data
+table = soup.find('table', class_='tablesorter')
+table_body = table.find('tbody')
+rows = table_body.find_all('tr')
+
+for row in rows:
+    cols = row.find_all('td')
+    date_time = cols[0].text.strip()
+    discharge_ft3_per_s = cols[1].text.strip()
+    disolved_oxygen_mg_per_L = cols[2].text.strip()
+    gege_height_feet = cols[3].text.strip()
+    temperature_water_degC = cols[4].text.strip()
+    specific_conductance_unfus_per_cm_at_25degC = cols[5].text.strip()
+    pH_water_unfltr_field_std_units = cols[6].text.strip()
+    turbidity_ir_led_light_det_ang_90_deg = cols[7].text.strip()
+    
+    # cols = [ele.text.strip() for ele in cols]
+    # data.append([ele for ele in cols if ele])
+    
+    # print column 
+    #print(date_time)
+   
+    # insert crawled data to table
+    # value = (date_time,discharge_ft3_per_s,disolved_oxygen_mg_per_L,gege_height_feet,temperature_water_degC,specific_conductance_unfus_per_cm_at_25degC,pH_water_unfltr_field_std_units,turbidity_ir_led_light_det_ang_90_deg)
+    # sql = "INSERT INTO usgs(date_time,discharge_ft3_per_s,disolved_oxygen_mg_per_L,gege_height_feet,temperature_water_degC,specific_conductance_unfus_per_cm_at_25degC,pH_water_unfltr_field_std_units,turbidity_ir_led_light_det_ang_90_deg) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(date_time,discharge_ft3_per_s,disolved_oxygen_mg_per_L,gege_height_feet,temperature_water_degC,specific_conductance_unfus_per_cm_at_25degC,pH_water_unfltr_field_std_units,turbidity_ir_led_light_det_ang_90_deg)
+    # mycursor.execute(sql)
+
+    
+
